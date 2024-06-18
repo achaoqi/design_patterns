da26e41e1018b43ad44e649b70b8b14707c51c94
smartchaoqi <qcq11111111@163.com>
2024-06-18 10:16:17 +0800

    责任链模式

---
+++ src/main/java/diagram/pattern/responsibility/Trouble.java
@@ -0,0 +1,17 @@
+package diagram.pattern.responsibility;
+
+public class Trouble {
+    private int number;
+    public Trouble(int number){
+        this.number=number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "[Trouble "+number+"]";
+    }
+}
